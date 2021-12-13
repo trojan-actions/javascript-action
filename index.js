dd1a5ea408f7a4d32c2f6659189cd69005538773
Gus SS <gusshawstewart@github.com>
2021-12-13 18:41:25 +0000

    test

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const github = require('@actions/github');
 
 try {
   console.log(github.context);
-  github.rest.createIssue({
-    owner: github.context.payload.repository.owner.login,
-    repo: github.context.payload.repository.name,
-    body: `Hi \n\nThis is a test action.\n\nTime: ${time}`
+  github.context.createIssue({
+    title: 'Hello world', 
+    body: 'This is a test issue'
   });
+  // github.rest.createIssue({
+  //   owner: github.context.payload.repository.owner.login,
+  //   repo: github.context.payload.repository.name,
+  //   body: `Hi \n\nThis is a test action.\n\nTime: ${time}`
+  // });
 } catch (error) {
   core.setFailed(error.message);
 }
