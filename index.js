6a54876da9328ff790aea151ea99f08c6b19f4dc
Gus SS <gusshawstewart@github.com>
2021-12-13 18:21:30 +0000

    test

--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Now using the root directory`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  github.context.payload.ref_type = 'branch';
+  console.log(repo: github.context.payload.repository.name);
+  github.rest.issues.createComment({
+    owner: github.context.payload.repository.owner.login,
+    repo: github.context.payload.repository.name,
+    issue_number: github.context.payload.issue.number,
+    body: `Hi ${nameToGreet}!\n\nThis is a test action.\n\nTime: ${time}`
+  });
 } catch (error) {
   core.setFailed(error.message);
 }
