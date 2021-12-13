164718ce820f882eb8d33bfab0dbaf4801512ddb
Gus SS <gusshawstewart@github.com>
2021-12-13 18:23:49 +0000

    test

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ try {
   github.rest.issues.createComment({
     owner: github.context.payload.repository.owner.login,
     repo: github.context.payload.repository.name,
-    issue_number: github.context.payload.issue.number,
-    body: `Hi ${nameToGreet}!\n\nThis is a test action.\n\nTime: ${time}`
+    body: `Hi \n\nThis is a test action.\n\nTime: ${time}`
   });
 } catch (error) {
   core.setFailed(error.message);
