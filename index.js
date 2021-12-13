40c300b6baa1b7fce1d8ab6b4463264c9f08efc5
Gus SS <gusshawstewart@github.com>
2021-12-13 18:19:21 +0000

    make a comment on the repo with github script

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+  github.context.payload.ref_type = 'branch';
 } catch (error) {
   core.setFailed(error.message);
 }
