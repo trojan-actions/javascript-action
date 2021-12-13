4a4796c1876d255ada27491e78a1b08127a04f94
Gus SS <gusshawstewart@github.com>
2021-12-13 18:54:12 +0000

    test

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const token = core.getInput('repo-token', { required: true })
+const octokit = github.getOctokit(token)
 
 try {
-  github.rest.issues.create({
-    owner: github.context.payload.repository.owner.login,
-    repo: github.context.payload.repository.name,
-    body: `Hi \n\nThis is a test action.\n\nTime: ${time}`
+
+  const { context } = github
+  const { owner, repo } = context.repo
+
+  octokit.issues.create({
+    owner,
+    repo,
+    title: `${tomorrow.format('YYYY-MM-DD')} Weekly Sync`,
+    labels: ['☑ Meeting Notes'],
+    assignees: ['GMAN']
   });
+
 } catch (error) {
   core.setFailed(error.message);
 }
